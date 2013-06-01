c61344a767b13e4d856e287967e1f27efc5fe508
Ely De La Cruz <elycruz@elycruz.com>
2013-06-01 10:16:19 -0400

    Menu Fieldset in place.

---
+++ module/Edm/src/Edm/Form/MenuFieldset.php
@@ -0,0 +1,145 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Edm\Form;
+
+use Zend\Form\Fieldset,
+    Edm\Model\Menu;
+
+/**
+ * Description of MenuFieldset
+ * @author ElyDeLaCruz
+ */
+class MenuFieldset extends Fieldset {
+
+    public function __construct($name = 'menu-fieldset', $options = array()) {
+
+        parent::__construct($name, $options);
+
+        // Post Object
+        $this->setObject(new Menu());
+
+        // Min Depth
+        $this->add(array(
+            'name' => 'minDepth',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Minimum Depth'
+            ),
+            'attributes' => array(
+                'id' => 'minDepth',
+                'required' => false,
+            )
+        ));
+        
+        // Max Depth
+        $this->add(array(
+            'name' => 'maxDepth',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Maximum Depth'
+            ),
+            'attributes' => array(
+                'id' => 'maxDepth',
+                'required' => false,
+            )
+        ));
+        
+        // Only Active Branch
+        $this->add(array(
+            'name' => 'onlyActiveBranch',
+            'type' => 'checkbox',
+            'options' => array(
+                'label' => 'Only Active Branch'
+            ),
+            'attributes' => array(
+                'id' => 'onlyActiveBranch',
+                'required' => false,
+            )
+        ));
+        
+        // Render Parents
+        $this->add(array(
+            'name' => 'renderParents',
+            'type' => 'checkbox',
+            'options' => array(
+                'label' => 'Render Parents'
+            ),
+            'attributes' => array(
+                'id' => 'renderParents',
+                'required' => false,
+            )
+        ));
+        
+        // Is Main Menu
+        $this->add(array(
+            'name' => 'isMainMenu',
+            'type' => 'checkbox',
+            'options' => array(
+                'label' => 'Is Main Menu'
+            ),
+            'attributes' => array(
+                'id' => 'isMainMenu',
+                'required' => false,
+            )
+        ));
+        
+        // Use Module Helper
+        $this->add(array(
+            'name' => 'useModuleHelper',
+            'type' => 'checkbox',
+            'options' => array(
+                'label' => 'Use Module Helper'
+            ),
+            'attributes' => array(
+                'id' => 'useModuleHelper',
+                'required' => false,
+            )
+        ));
+        
+        // Ul Class
+        $this->add(array(
+            'name' => 'ulClass',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Ul Class'
+            ),
+            'attributes' => array(
+                'id' => 'ulClass',
+                'required' => false,
+            )
+        ));
+        
+        // Menu Partial Script
+        $this->add(array(
+            'name' => 'menuPartialScript',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Menu Partial Script'
+            ),
+            'attributes' => array(
+                'id' => 'menuPartialScript',
+                'required' => false,
+            )
+        ));
+        
+        // Menu Helper
+        $this->add(array(
+            'name' => 'menuHelper',
+            'type' => 'text',
+            'options' => array(
+                'label' => 'Menu Helper'
+            ),
+            'attributes' => array(
+                'id' => 'menuHelper',
+                'required' => false,
+            )
+        ));
+     
+    }
+    
+}
