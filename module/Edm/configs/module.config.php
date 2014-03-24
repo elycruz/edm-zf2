d5506083c93925f13d37669ec895f2c61dca6785
Ely De La Cruz <elycruz@elycruz.com>
2014-03-23 23:05:58 -0400

    Changed edm-ko-ui2 path to edm-polymer-ui.

--- module/Edm/configs/module.config.php
+++ module/Edm/configs/module.config.php
@@ -203,8 +203,8 @@ return array(
             'Edm' => __DIR__ . '/../view-scripts',
         ),
         'template_map' => array(
-            'layout/edm-admin-login'     => APP_PATH .'/public/module-templates/edm-ko-ui/login.phtml',
-            'layout/edm-admin-ajax-ui'  => APP_PATH . '/public/module-templates/edm-ko-ui2/index.phtml'
+            'layout/edm-admin-login'    => APP_PATH .'/public/module-templates/edm-ko-ui/login.phtml',
+            'layout/edm-admin-ajax-ui'  => APP_PATH . '/public/module-templates/edm-polymer-ui/index.phtml'
         )
     ),
 );
