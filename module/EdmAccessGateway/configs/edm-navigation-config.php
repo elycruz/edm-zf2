2d2f2f5932fdbd26c2603fd692272308938b2bc5
Ely De La Cruz <elycruz@elycruz.com>
2014-03-23 20:07:40 -0400

    Updated edm-navigation-config.php to not show the sub index  and sub delete links as these are dynamically used by the frontend through code.

--- module/EdmAccessGateway/configs/edm-navigation-config.php
+++ module/EdmAccessGateway/configs/edm-navigation-config.php
@@ -1,76 +1,83 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Edm Admin Navigation config
+ * @type array
  */
-
 return array(
     'default' => array(
         array(
-            'label' => 'Terms',
-            'route' => 'edm-admin/term',
-            'resource' => 'term',
-            'privilege' => 'cms-super',
+            'label' => 'Content',
+            'uri' => '/edm-admin/content',
             'pages' => array(
                 array(
-                    'label' => 'Term Index',
-                    'route' => 'edm-admin/term',
-                    'privilege' => 'cms-user',
-                    'resource' => 'index'
-                ),
-                array(
-                    'label' => 'Term Create',
-                    'route' => 'edm-admin/term/create',
+                    'label' => 'Posts',
+                    'route' => 'paginator',
+                    'resource' => 'post',
                     'privilege' => 'cms-author',
-                    'resource' => 'create'
-                ),
-                array(
-                    'label' => 'Term Update',
-                    'route' => 'edm-admin/term/update',
-                    'privilege' => 'cms-author',
-                    'resource' => 'update'
-                ),
-                array(
-                    'label' => 'Term Delete',
-                    'route' => 'edm-admin/term/delete',
-                    'privilege' => 'cms-author',
-                    'resource' => 'delete'
+                    'controller' => 'post',
+                    'action' => 'index',
+                    'pages' => array(
+                        array(
+                            'label' => 'Post Create',
+                            'route' => 'edm-admin-default',
+                            'privilege' => 'cms-author',
+                            'resource' => 'create'
+                        )
+                    )
                 ),
             )
         ),
         array(
-            'label' => 'Term Taxonomies',
-            'route' => 'edm-admin/term-taxonomy',
-            'privilege' => 'cms-user',
-            'resource' => 'term-taxonomy',
+            'label' => 'System',
+            'privilege' => 'cms-super',
+            'uri' => '',
             'pages' => array(
                 array(
-                    'label' => 'Term Taxonomy Index',
+                    'label' => 'Terms',
                     'route' => 'edm-admin/term',
+                    'resource' => 'term',
+                    'privilege' => 'cms-super',
+                    'controller' => 'TermController',
+                    'pages' => array(
+                        array(
+                            'label' => 'Term Create',
+                            'route' => 'edm-admin-default',
+                            'privilege' => 'cms-author',
+                            'resource' => 'create'
+                        ),
+                    )
+                ),
+                array(
+                    'label' => 'Term Taxonomies',
+                    'route' => 'edm-admin/term-taxonomy',
                     'privilege' => 'cms-user',
-                    'resource' => 'index'
+                    'resource' => 'term-taxonomy',
+                    'pages' => array(
+                        array(
+                            'label' => 'Term Taxonomy Create',
+                            'route' => 'updateOrDelete',
+                            'privilege' => 'cms-author',
+                            'resource' => 'create'
+                        ),
+                    )
                 ),
                 array(
-                    'label' => 'Term Taxonomy Create',
-                    'route' => 'edm-admin/term-taxonomy/create',
-                    'privilege' => 'cms-author',
-                    'resource' => 'create'
+                    'label' => 'Users',
+                    'route' => 'edm-admin/user',
+                    'privilege' => 'cms-user',
+                    'resource' => 'user',
+                    'pages' => array(
+                        array(
+                            'label' => 'User Create',
+                            'route' => 'edm-admin/user/create',
+                            'privilege' => 'cms-manager',
+                            'resource' => 'create'
+                        ),
+                    )
                 ),
-                array(
-                    'label' => 'Term Taxonomy Update',
-                    'route' => 'edm-admin/term-taxonomy/update',
-                    'privilege' => 'cms-author',
-                    'resource' => 'update'
-                ),
-                array(
-                    'label' => 'Term Taxonomy Delete',
-                    'route' => 'edm-admin/term-taxonomy/delete',
-                    'privilege' => 'cms-author',
-                    'resource' => 'delete'
-                ),
-            )
-        ),
-    ),
+                
+            ),
+        )
+    )
 );
